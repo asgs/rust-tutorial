dd47debbeea943df69e28d689805c38d067a56ae
asgs <asgosa.eeegct@gmail.com>
2019-04-21 00:13:44 +0530

    Slightly refactor Fibonacci-related functions

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -1,17 +1,21 @@
 fn main() {
+    fib();
+}
+
+fn fib() {
     let nth_fib = 25;
     let fib_num = if nth_fib == 0 || nth_fib == 1 {
                      1
                  } else {
-                     fibonacci(nth_fib)
+                     calc_fib(nth_fib)
                  };
     println!("{}th Fibonacci is {}.", nth_fib, fib_num);                    
 }
 
-fn fibonacci(x: i32) -> i32 {
+fn calc_fib(x: i32) -> i32 {
    if x == 1 || x == 2 {
       1
    } else {
-      fibonacci(x - 1) + fibonacci(x - 2)
+      calc_fib(x - 1) + calc_fib(x - 2)
    }
 }
